ae6f324ffaadede4687afbfac7d4f596ab9f7b8d
Zac <YorkshireDev@protonmail.com>
2023-06-03 16:40:53 +0100

    refactor: Set up all GUI components including i8n

--- src/view/ViewMain.java
+++ src/view/ViewMain.java
@@ -6,8 +6,28 @@ import java.awt.*;
 public class ViewMain extends JFrame {
 
     private JPanel panelMain;
+    private JLabel labelGithubUrl;
+    private JLabel labelTimeRemaining;
+    private JButton buttonStartStop;
+    private JCheckBox checkBoxRunForever;
+    private JCheckBox checkBoxSaveFindings;
+    private JLabel labelTimeToRun;
+    private JLabel labelThreadCount;
+    private JTextField textFieldTimeToRun;
+    private JTextField textFieldThreadCount;
+    private JList<String> listResults;
+    private JLabel labelResults;
+    private DefaultListModel<String> listResultsModel;
 
     private void init() {
+
+        textFieldTimeToRun.setText("60");
+        textFieldThreadCount.setText(String.valueOf(Runtime.getRuntime().availableProcessors()));
+
+        DefaultListCellRenderer listResultsRenderer = (DefaultListCellRenderer) listResults.getCellRenderer();
+        listResultsRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+        listResultsModel = new DefaultListModel<>();
+        listResults.setModel(listResultsModel);
 
         setContentPane(panelMain);
         setTitle("Mersenne");
